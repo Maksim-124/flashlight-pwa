3a9f720d0435811831bb8bf237e4216463ea9037
Maksim-124 <Bocman024@mail.ru>
2025-10-25 16:07:42 +0300

    Удален .nojekyll, исправлены пути CSS

--- components/themes/warhammer-theme.js
+++ components/themes/warhammer-theme.js
@@ -21,7 +21,7 @@ class WarhammerTheme extends HTMLElement {
     render() {
         this.shadowRoot.innerHTML = `
             <style>
-                @import "https://maksim-124.github.io/flashlight-pwa/styles/themes/warhammer-theme.css";
+                @import "../../styles/themes/warhammer-theme.css";
             </style>
             
             <div class="light-overlay"></div>
